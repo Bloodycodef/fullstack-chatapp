c837e50a0dabe28ee214f3767f647edba4df9245
BloodyCodef <gedewahyuspidey@gmail.com>
2025-01-28 00:57:05 +0800

    versi 1.6

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? `${import.meta.env.VITE_REACT_APP_API_URL}` : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://fullstack-chatapp-9f79.vercel.app/api" : "/api",
   withCredentials: true,
 });
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? `${import.meta.env.VITE_REACT_APP_API_URL}` : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "https://fullstack-chatapp-9f79.vercel.app/api" : "/api";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
